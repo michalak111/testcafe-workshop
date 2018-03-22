72464902f7210dfa6243f1dd6924e8c9833d09fc
michalak111 <michalak111@gmail.com>
2018-03-22 09:35:01 +0100

    TestCafe: - login class

--- src/app.e2e.js
+++ src/app.e2e.js
@@ -4,11 +4,30 @@ fixture('Login').page('http://localhost:1234/')
 
 const loginStatus = Selector('.help')
 
-test('Login', async (t) => {
-  await t
-    .typeText('#email', 'test@test.com')
-    .typeText('#password', 'qwe')
-    .pressKey('enter')
+class Login {
+  constructor({ email = 'test@test.com', password = 'qwe' } = {}) {
+    this.email = email
+    this.password = password
+  }
+  async login() {
+    await t
+      .typeText('#email', this.email)
+      .typeText('#password', this.password)
+      .pressKey('enter')
+  }
+}
 
+test('Login', async (t) => {
+  await new Login().login()
   await t.expect(loginStatus.innerText).eql('LoginSuccess')
+})
+
+test('Login - wrong password', async (t) => {
+  await new Login({ password: 'poop' }).login()
+  await t.expect(loginStatus.innerText).eql('WrongPassword')
+})
+
+test('Login - wrong email', async (t) => {
+  await new Login({ email: 'poop@test.com' }).login()
+  await t.expect(loginStatus.innerText).eql('WrongEmail')
 })
